23c6cad011788f0322949b641f108ba4810ef057
Leon Malisov <leonmalisov@gmail.com>
2019-11-28 02:12:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,4 +8,4 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-board = [" Z ", " X ", "   ", "   ", " X ", "   ", "   ", " X ", "   ", ]
+board = ["  ", " X ", "   ", "   ", " X ", "   ", "   ", " X ", "   ", ]
