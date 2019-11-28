af4986257ae8a37bcd7055fdec82cc90e229f60b
Leon Malisov <leonmalisov@gmail.com>
2019-11-28 02:04:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  puts " #{board[1]} | #{board[1]} | #{board[1]} "
+  puts "-----------"
+  puts " #{board[1]} | #{board[1]} | #{board[1]} "
+  puts "-----------"
+  puts " #{board[1]} | #{board[1]} | #{board[1]} "  
